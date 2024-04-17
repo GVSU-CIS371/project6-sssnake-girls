a68f62e00b451fdae72a17cbf4c0e089e686afe2
jcdotcom <jwexler101@gmail.com>
2024-04-17 13:28:00 -0400

    more firestore integration

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -1,7 +1,14 @@
 import { defineStore } from "pinia"
 import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
-import { initializeApp } from "firebase/app";
+import { initializeApp, FirebaseApp } from "firebase/app";
+import {    getFirestore,
+            Firestore,
+            getDocs,
+            collection,
+            QuerySnapshot,
+            QueryDocumentSnapshot
+        } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -15,6 +22,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db: Firestore = getFirestore(app);
 
 export const useItemStore = defineStore("ItemStore", {
     state: () => ({
@@ -26,10 +34,25 @@ export const useItemStore = defineStore("ItemStore", {
             this.count++
         },
         init(){
-            
+            const collectionRef = collection(db, 'Products'); // Replace 'products' with your collection name
+            getDocs(collectionRef).then((qs: QuerySnapshot) => {
+                if (qs.size === 0) {
+                    // Collection is empty, initialize with default data
+                    this.products = initProducts;
+                } else {
+                    const fetchedProducts: ProductDoc[] = [];
+                    qs.forEach((qd: QueryDocumentSnapshot) => {
+                        const productData = qd.data() as ProductDoc;
+                        const docId = qd.id;
+                        fetchedProducts.push({ ...productData, id: docId });
+                    });
+                    this.products = fetchedProducts;
+                }
+            }).catch((error) => {
+                console.error("Error checking if collection is empty: ", error);
+            });
             //TODO: make initProducts run if firestore empty, otherwise load firebase
-            
-            this.products = initProducts;
+            //this.products = initProducts;
         },
         filterByCategory(cat: string){
             this.products = initProducts.filter((prod) => prod.data.category == cat)
